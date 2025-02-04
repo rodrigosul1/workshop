c4eb86ce4fa01a9157f1cb6ef28b9366076cc05f
Rodrigo Sul <rodrigosul10@gmail.com>
2025-02-03 21:25:59 -0300

    Add form validation and UI component libraries
    - Integrated React Hook Form with @hookform/resolvers for form handling
    - Added Radix UI primitives for improved component composition
    - Updated package dependencies to support form validation and UI components

---
+++ src/components/forms/signin-form.tsx
@@ -0,0 +1,76 @@
+import { cn } from "@/lib/utils"
+import { z } from "zod";
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+const signinFormSchema = z.object({
+    username: z.string().min(1, { message: "Nome de usuário é obrigatório" }),
+    password: z.string().min(1, { message: "Senha é obrigatório" }),
+  });
+  
+  type SigninFormValues = z.infer<typeof signinFormSchema>;
+
+export function SignInForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <Button variant="outline" className="w-full">
+                Login with Google
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
